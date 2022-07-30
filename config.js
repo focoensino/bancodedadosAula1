60389332d3944959034a1b616be3e35b813e18c0
Sérgio Tafine <focoensinoonline@gmail.com>
2022-07-29 22:32:51 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC6j8SztsCJZW6tzu82ZX4llbPTGasZYGI",
+      authDomain: "jogo-de-carros-1585f.firebaseapp.com",
+      databaseURL: "https://jogo-de-carros-1585f-default-rtdb.firebaseio.com",
+      projectId: "jogo-de-carros-1585f",
+      storageBucket: "jogo-de-carros-1585f.appspot.com",
+      messagingSenderId: "1047771128481",
+      appId: "1:1047771128481:web:6a52d94b7d482d484986e0"
+  };
